0bd5cb2b266ab86501828fabd7e04e8b5c0cfa09
indykoning <15870933+indykoning@users.noreply.github.com>
2022-07-05 17:29:55 +0200

    Dont cache html if template contains pushes

--- src/ViewComponents/SnowdogMenuComponent.php
+++ src/ViewComponents/SnowdogMenuComponent.php
@@ -19,14 +19,38 @@ class SnowdogMenuComponent extends Component
     {
         $identifier = $this->identifier;
 
-        return Cache::rememberForever('snowdogmenu.'.config('rapidez.store').'.'.$identifier, function () use ($identifier) {
+        $templateData = Cache::rememberForever('snowdogmenu.'.config('rapidez.store').'.'.$identifier, function () use ($identifier) {
             $menu = Menu::where('identifier', $identifier)->firstOrFail();
 
-            return view('snowdogmenu::level1', [
+            $view = view('snowdogmenu::level1', [
                 'items'      => $this->convertToMenuTree($menu->items),
                 'identifier' => $identifier,
-            ])->render();
+            ]);
+
+            // Trick to view the protected "pushes" attribute.
+            // If it has items in the stack, we must not cache
+            // the rendered view as we'll lose our push stacks.
+            $viewFactory = $view->getFactory();
+            $pushes = (new \ReflectionClass($viewFactory))->getProperty('pushes');
+            $pushes->setAccessible(true);
+
+            if(count($pushes->getValue($viewFactory))) {
+                return ['items' => $items];
+            }
+
+            // Immediately returning html is faster.
+            // So return html if no stacks.
+            return $view->render();
         });
+
+        if (is_string($templateData)) {
+            return $templateData;
+        }
+
+        return view('snowdogmenu::level1', [
+            'items'      => $templateData['items'],
+            'identifier' => $identifier,
+        ]);
     }
 
     protected function convertToMenuTree($items, $parentId = null)
