4c9ed09d8bd8e1eb976c070d295b5012da605c0a
indykoning <15870933+indykoning@users.noreply.github.com>
2023-07-04 10:32:03 +0200

    Use url helpers (#5)

--- resources/views/level3.blade.php
+++ resources/views/level3.blade.php
@@ -1,7 +1,7 @@
 <ul class="text-sm">
     @foreach ($items as $item)
         <li>
-            <a class="inline-block py-1 hover:text-primary {{ $item->classes }}" href="{{ $item->content }}">
+            <a class="inline-block py-1 hover:text-primary {{ $item->classes }}" href="{{ url($item->content) }}">
                 {{ $item->title }}
             </a>
         </li>
